76528d8178b68f5441603f6de0eeb84a46f18a9e
libingchen <3952700@qq.com>
2018-08-05 10:30:14 +0800

    order_insert_recvwindow_ms,order_action_recvwindow_ms  parameterization

--- wingchun/td/TDEngineBinance.h
+++ wingchun/td/TDEngineBinance.h
@@ -148,7 +148,8 @@ private:
     ThreadPtr rest_thread;
     uint64_t last_rest_get_ts = 0;
     uint64_t rest_get_interval_ms = 500;
-
+    uint64_t order_insert_recvwindow_ms = 5000;
+    uint64_t order_action_recvwindow_ms = 5000;
     std::mutex* mutex_order_and_trade = nullptr;
 
     int SYNC_TIME_DEFAULT_INTERVAL = 10000;
