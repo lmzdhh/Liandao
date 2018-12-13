10e24d1818538c44ea47650ee8e84b472466bc12
lck <343070935@qq.com>
2018-12-13 23:16:49 +0800

    error

--- wingchun/md/MDEngineOceanEx.cpp
+++ wingchun/md/MDEngineOceanEx.cpp
@@ -228,10 +228,10 @@ void MDEngineOceanEx::readWhiteLists(const json& j_config)
                 */
                     std::string strMarketSub = makeMarketSub(it->second,book_depth_count);
                     websocketSubscribeJsonString.push_back(std::move(strMarketSub);
-                    KF_LOG_INFO(logger, "[MDEngineOceanEx::readWhiteLists] makeMarketSub: " << strMarketSub;
+                    KF_LOG_INFO(logger, "[MDEngineOceanEx::readWhiteLists] makeMarketSub: " << strMarketSub);
                     std::string strTradeSub = makeTradeSub(it->second);
                     websocketSubscribeJsonString.push_back(std::move(strTradeSub);
-                     KF_LOG_INFO(logger, "[MDEngineOceanEx::readWhiteLists] makeTradeSub: " << strTradeSub;
+                     KF_LOG_INFO(logger, "[MDEngineOceanEx::readWhiteLists] makeTradeSub: " << strTradeSub);
 			}
 		}
 	}
