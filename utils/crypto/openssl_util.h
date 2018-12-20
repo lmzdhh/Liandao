55d689b06730e76fbd0f53d569143d76ee678202
lck <343070935@qq.com>
2018-12-21 00:33:43 +0800

    jwt

--- utils/crypto/openssl_util.h
+++ utils/crypto/openssl_util.h
@@ -130,4 +130,192 @@ std::string base64_decode(std::string const& encoded_string) {
     return ret;
 }
 
+std::string base64_url_encode(const unsigned char* bytes_to_encode, unsigned long in_len) {
+	std::string ret;
+	int i = 0;
+	int j = 0;
+	unsigned char char_array_3[3];
+	unsigned char char_array_4[4];
+
+	while (in_len--) {
+		char_array_3[i++] = *(bytes_to_encode++);
+		if (i == 3) {
+			char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+			char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+			char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+			char_array_4[3] = char_array_3[2] & 0x3f;
+
+			for (i = 0; (i <4); i++)
+				ret += base64_url_chars[char_array_4[i]];
+			i = 0;
+		}
+	}
+
+	if (i)
+	{
+		for (j = i; j < 3; j++)
+			char_array_3[j] = '\0';
+
+		char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+		char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+		char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+
+		for (j = 0; (j < i + 1); j++)
+			ret += base64_url_chars[char_array_4[j]];
+
+		//while((i++ < 3))
+		//ret += '=';
+
+	}
+
+	return ret;
+
+}
+
+std::string base64_url_decode(std::string const& encoded_string) {
+	int in_len = encoded_string.size();
+	int i = 0;
+	int j = 0;
+	int in_ = 0;
+	unsigned char char_array_4[4], char_array_3[3];
+	std::string ret;
+
+	while (in_len-- && (encoded_string[in_] != '=') && is_base64_url(encoded_string[in_])) {
+		char_array_4[i++] = encoded_string[in_]; in_++;
+		if (i == 4) {
+			for (i = 0; i <4; i++)
+				char_array_4[i] = base64_url_chars.find(char_array_4[i]);
+
+			char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+			char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+			char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+			for (i = 0; (i < 3); i++)
+				ret += char_array_3[i];
+			i = 0;
+		}
+	}
+
+	if (i) {
+		for (j = 0; j < i; j++)
+			char_array_4[j] = base64_url_chars.find(char_array_4[j]);
+
+		char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+		char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+
+		for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
+	}
+
+	return ret;
+}
+
+
+inline std::string rsa256_private_sign(const std::string &data_to_sign, const std::string &priKey) {
+    std::string strRet;
+    unsigned int len=0;
+    BIO *keybio = BIO_new(BIO_s_mem());
+
+    size_t  ret = BIO_write(keybio,priKey.data(),priKey.size());
+    if(ret != priKey.size())
+    {
+        return "";
+    }
+    std::string password ="";
+    RSA *rsa = PEM_read_bio_RSAPrivateKey(keybio, nullptr , nullptr, (void*)password.c_str());
+    if(rsa == nullptr)
+    {
+        return  "";
+    }
+    EVP_PKEY* key = EVP_PKEY_new();
+
+    EVP_PKEY_assign_RSA(key,rsa);
+    strRet.resize(EVP_PKEY_size(key));
+
+    EVP_MD_CTX* ctx = EVP_MD_CTX_create();
+    EVP_SignInit(ctx,EVP_sha256());
+    EVP_SignUpdate(ctx,data_to_sign.c_str(),data_to_sign.length());
+    EVP_SignFinal(ctx,(unsigned char*)strRet.data(),&len,key);
+    strRet.resize(len);
+
+    EVP_MD_CTX_destroy(ctx);
+    //RSA_free(rsa);
+    EVP_PKEY_free(key);
+    BIO_free_all(keybio);
+
+    return strRet;
+}
+
+
+
+inline std::string rsa256_pub_verify(const std::string &data_to_sign,const std::string &cipherText, const std::string &pubKey)
+{
+    std::string strRet;
+    BIO *keybio = BIO_new(BIO_s_mem());
+
+    size_t  ret = BIO_write(keybio,pubKey.data(),pubKey.size());
+    if(ret != pubKey.size())
+    {
+        strRet = "failed to load public key: bio_write failed";
+        return strRet;
+    }
+    std::string password ="";
+    RSA *rsa = PEM_read_bio_RSAPublicKey(keybio, nullptr , nullptr, (void*)password.c_str());
+    if(rsa == nullptr)
+    {
+        strRet = "failed to load public key: PEM_read_bio_PUBKEY failed";
+        return strRet;
+    }
+    EVP_PKEY* key = EVP_PKEY_new();
+
+    EVP_PKEY_assign_RSA(key,rsa);
+    EVP_MD_CTX* ctx =EVP_MD_CTX_create();
+
+    if (!ctx)
+    {
+        strRet = "failed to verify signature: could not create context";
+        return strRet;
+    }
+    if (!EVP_VerifyInit(ctx, EVP_sha256()))
+    {
+        strRet = "failed to verify signature: VerifyInit failed";
+        return strRet;
+    }
+    if (!EVP_VerifyUpdate(ctx, data_to_sign.data(), data_to_sign.size()))
+    {
+        strRet = "failed to verify signature: VerifyUpdate failed";
+        return strRet;
+    }
+    if (!EVP_VerifyFinal(ctx, (const unsigned char*)cipherText.data(), cipherText.size(), key))
+    {
+        strRet = "failed to load public key: PEM_read_bio_PUBKEY failed";
+        return strRet;
+    }
+
+
+    EVP_MD_CTX_destroy(ctx);
+    //RSA_free(rsa);
+    EVP_PKEY_free(key);
+    BIO_free_all(keybio);
+
+    return strRet;
+}
+
+inline std::string jwt_create(const std::string& data,const std::string& private_key)
+{
+    //JWT:
+    //1. secret =  RSASHA256(base64UrlEncode(header) + "." + base64UrlEncode(payload),private_key)
+    //2. jwt = base64UrlEncode(header) + "." + base64UrlEncode(payload) +base64UrlEncode(secret)
+    std::string header =R"({"alg":"RS256","typ":"JWT"})";
+    std::string payload = data;
+
+    std::string encoded_header = base64_url_encode((const unsigned char*)header.c_str(),header.length());
+    std::string encoded_payload=base64_url_encode((const unsigned char*)payload.c_str(),payload.length());
+    std::string data_to_sign = encoded_header +"."+encoded_payload;
+    std::string signature = rsa256_private_sign(data_to_sign, private_key);
+    std::string secret = base64_url_encode((const unsigned char*)signature.c_str(),signature.length());
+
+    std::string jwt = data_to_sign+"."+secret;
+    return  jwt;
+}
+
 }}
