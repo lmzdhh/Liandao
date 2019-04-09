78210dc94c55eb4a1d4818d9523c66cd5360bb0c
FFXN <468517235@qq.com>
2019-04-09 17:47:27 +0800

    const

--- wingchun/td/TDEngineBinance.cpp
+++ wingchun/td/TDEngineBinance.cpp
@@ -635,7 +635,8 @@ void TDEngineBinance::req_order_insert(const LFInputOrderField* data, int accoun
     if (UFR_data_map.find(data->InstrumentID) == UFR_data_map.end())
     {
         uint64_t UFRnums[2] = {0,0};
-        UFR_data_map.insert(std::make_pair(data->InstrumentID, UFRnums));
+        char_31 instrument_ID = data->InstrumentID;
+        UFR_data_map.insert(std::make_pair(instrument_ID, UFRnums));
 
         //测试日志
         KF_LOG_DEBUG(logger, "[req_order_insert] add InstrumentID into UFR_data_map  instrumentID " << 
