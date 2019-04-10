3b1019988a85fd6f65c038fc1c89ac88c99fe17c
SS <sunzhengxiao@sina.com>
2019-04-10 16:18:57 +0800

    add "post_only" option in tag

--- wingchun/strategy/py_demo/binance_pricebook_test.py
+++ wingchun/strategy/py_demo/binance_pricebook_test.py
@@ -22,7 +22,7 @@ wingchun strategy -n my_test -p binance_order_cancel_test.py
 
 def initialize(context):
     context.add_md(source=SOURCE.BINANCE)
-    context.ticker = 'trx_btc'
+    context.ticker = 'btc_usdt'
     context.exchange_id = EXCHANGE.SHFE
     context.buy_price = -1
     context.sell_price = -1
@@ -36,3 +36,4 @@ def on_price_book(context, price_book, source, rcv_time):
     print 'ExchangeID' , price_book.ExchangeID
     print 'BidLevels' , price_book.BidLevels
     print 'AskLevels' , price_book.BidLevels
+    
