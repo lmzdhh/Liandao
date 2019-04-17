64ef2be1c1e67489afe272707af8782c0ba636b9
Cays <1574411019@qq.com>
2019-04-17 15:41:43 +0800

    cys add price and volumn precision

--- wingchun/td/TDEngineHuobi.cpp
+++ wingchun/td/TDEngineHuobi.cpp
@@ -987,9 +987,9 @@ void TDEngineHuobi::dealPriceVolume(AccountUnitHuobi& unit,const std::string& sy
         long long lDealVolume=tDealVolume*pow(10,vPrecision);
         tDealVolume=lDealVolume*1.0/pow(10,vPrecision);
         nDealPrice=std::to_string(tDealPrice);
-        nDealPrice=nDealPrice.substr(0,nDealPrice.find(".")+pPrecision);
+        nDealPrice=nDealPrice.substr(0,nDealPrice.find(".")+pPrecision+1);
         nDealVolume=std::to_string(tDealVolume);
-        nDealVolume=nDealVolume.substr(0,nDealVolume.find(".")+vPrecision);
+        nDealVolume=nDealVolume.substr(0,nDealVolume.find(".")+vPrecision+1);
         KF_LOG_INFO(logger,"[dealPriceVolume] (nDealPrice) "<<nDealPrice <<" (nDealVolume) "<<nDealVolume);
     }
     KF_LOG_INFO(logger, "[dealPriceVolume]  (symbol)" << ticker << " (Volume)" << nVolume << " (Price)" << nPrice
