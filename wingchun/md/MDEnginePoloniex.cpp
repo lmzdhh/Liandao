b60a2411f2b56ef96d09f73b780759e1fc4f32b4
Strulch <865410940@qq.com>
2019-05-13 14:35:47 +0800

    debug

--- wingchun/md/MDEnginePoloniex.cpp
+++ wingchun/md/MDEnginePoloniex.cpp
@@ -344,7 +344,7 @@ void MDEnginePoloniex::GetINitializationInfomation(Document& json, int channlId,
         debug_print(websocketSubscribeChannel);
 
         for(auto& m : json.GetArray()[2].GetArray()[0].GetArray()[1]["orderBook"].GetArray()[0].GetObject()){
-            priceBook20Assembler.UpdateAskPrice(ticker,ticker,std::round(std::stod(m.name.GetString())*scale_offset),std::round(std::stod(m.value.GetString())*scale_offset));
+            priceBook20Assembler.UpdateAskPrice(ticker,std::round(std::stod(m.name.GetString())*scale_offset),std::round(std::stod(m.value.GetString())*scale_offset));
             KF_LOG_INFO(logger, "MDEnginePoloniex::onDepth: on_price_book_update : jsonAsk : price :"<<m.name.GetString()<<"   value :"<<m.value.GetString());
         }
 
